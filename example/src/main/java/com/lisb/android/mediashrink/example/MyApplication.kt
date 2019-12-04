7c713b68a11fa4c61041c4c674086cdc0cc26e00
tsunehito.takara <tsunehito.takara@l-is-b.com>
2019-12-04 14:13:02 +0900

    MyApplicationをKotlin化

---
+++ example/src/main/java/com/lisb/android/mediashrink/example/MyApplication.kt
@@ -0,0 +1,12 @@
+package com.lisb.android.mediashrink.example
+
+import android.app.Application
+import timber.log.Timber
+import timber.log.Timber.DebugTree
+
+class MyApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        Timber.plant(DebugTree())
+    }
+}
