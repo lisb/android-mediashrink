a3644d3f375c16fa204d7487eda821cdf0c5167f
tsunehito.takara <tsunehito.takara@l-is-b.com>
2019-12-05 11:35:10 +0900

    EncoderCreationExceptionをKotlin化

---
+++ library/src/main/java/com/lisb/android/mediashrink/EncoderCreationException.kt
@@ -0,0 +1,7 @@
+package com.lisb.android.mediashrink
+
+class EncoderCreationException(detailMessage: String?, throwable: Throwable?) : Exception(detailMessage, throwable) {
+    companion object {
+        private const val serialVersionUID = -2376268783992794022L
+    }
+}
