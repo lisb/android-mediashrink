a99b3c1e7dac03f0fa6cfcb7396e4a539968178f
tsunehito.takara <tsunehito.takara@l-is-b.com>
2019-12-05 11:14:51 +0900

    DecoderCreationExceptionをKotlin化

---
+++ library/src/main/java/com/lisb/android/mediashrink/DecoderCreationException.kt
@@ -0,0 +1,7 @@
+package com.lisb.android.mediashrink
+
+class DecoderCreationException(detailMessage: String?, throwable: Throwable?) : Exception(detailMessage, throwable) {
+    companion object {
+        private const val serialVersionUID = -1447299098199262989L
+    }
+}
